635befacee0280eed43639c02fe6bd384d8fc0ed
pawletakamil <37067611+pawletakamil@users.noreply.github.com>
2020-11-03 13:55:41 +0100

    File deploy

--- wp-content/themes/sporting-solutions/webpack/src/scripts/index.js
+++ wp-content/themes/sporting-solutions/webpack/src/scripts/index.js
@@ -1,55 +1,6 @@
-import '../styles/index.scss';
-import 'slick-carousel/slick/slick';
-import 'bootstrap';
-import functions from './functions.js';
-import animation from './animations.js';
-
-class Main {
-    constructor() {
-        this.onDOMLoaded();
-        this.onDocumentLoad();
-        this.onResize();
-        this.onScroll();
-        window.jQuery = window.$ = $;
-    }
-
-    onDocumentLoad() {
-        window.addEventListener("load", (event) => {
-
-        });
-    }
-
-    onDOMLoaded() {
-        document.addEventListener("DOMContentLoaded", (event) => {
-            functions.cookies();
-            functions.mobileToggle();
-            functions.iconSlider();
-            functions.showPerson();
-            functions.showMap();
-            functions.checkRequest();
-            functions.menuScroll();
-            functions.createOrangeUnderline();
-            functions.checkFormRadioButtons();
-            functions.scrollArrow();
-            functions.worldMapChange();
-            animation.animateHomepage();
-        });
-
-    }
-
-    onResize() {
-        window.addEventListener('resize', (event) => {
-
-        });
-    }
-    onScroll() {
-        window.addEventListener('scroll', (event) => {
-
-        });
-    }
-
-}
-
+import '../styles/index.scss';import 'slick-carousel/slick/slick';import 'bootstrap';import functions from './functions.js';import animation from './animations.js';class Main{constructor(){this.onDOMLoaded();this.onDocumentLoad();this.onResize();this.onScroll();window.jQuery=window.$=$;}
+onDocumentLoad(){window.addEventListener("load",(event)=>{});}
+onDOMLoaded(){document.addEventListener("DOMContentLoaded",(event)=>{functions.cookies();functions.mobileToggle();functions.iconSlider();functions.showPerson();functions.showMap();functions.checkRequest();functions.menuScroll();functions.createOrangeUnderline();functions.checkFormRadioButtons();functions.scrollArrow();functions.worldMapChange();animation.animateHomepage();});}
+onResize(){window.addEventListener('resize',(event)=>{});}
+onScroll(){window.addEventListener('scroll',(event)=>{});}}
 new Main();
-
-
